3ca0427392caca5f70f20baf66eb4e8c1b0ad23d
DUY LÊ MINH <24127027@student.hcmus.edu.vn>
2025-10-13 16:21:05 +0700

    done ex3

---
+++ exercise3.py
@@ -0,0 +1,9 @@
+def exercise3():
+    a = int(input("Nhập số nguyên thứ nhất: "))
+    b = int(input("Nhập số nguyên thứ hai: "))
+    c = a + b
+    print("Tổng của hai số nguyên là:", c)
+    return c
+
+if __name__ == "__main__":
+    exercise3()
