927a938519ef11d85abb57799b6ed081bed29de6
24127254 <24127254@student.hcmus.edu.vn>
2025-10-13 16:32:43 +0700

    Update 3.5.py

--- 3.5.py
+++ 3.5.py
@@ -1,3 +1,13 @@
+def printFunction1() -> str:
+    return "I'm a student."
+
+def printFunction2()-> str:
+    ans = round(1/7,5)
+    return str(ans)
+
+def sum2Num (num1 : int, num2 : int) -> int:
+    return num1 + num2
+
 def readFile(fileName: str) -> str:
     content = ""
     try:
@@ -6,10 +16,17 @@ def readFile(fileName: str) -> str:
     except FileNotFoundError:
         content = "File not found."
     return content
+
 def main():
+    bt1 = printFunction1()
+    bt2 = printFunction2()
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    result = sum2Num(num1, num2)
     fileName = input("Enter the file name: ")
     content = readFile(fileName)
-    open ("output.txt", "w").write(content)
+    with open ("output.txt", "w", encoding="utf-8") as file:
+        file.write(f"{bt1}\n{bt2}\nThe sum of {num1} and {num2} is {result}\nFile content: {content}")
 
 if __name__ == "__main__":
     main()
